cf92fc1d0eb108e1a7b23576eb8c2b2154d3764c
Kemoel Amorim Miranda <kemoel.amorim@gmail.com>
2023-09-23 16:48:07 -0300

    Remove linha duplicada

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class OlaDevDojo {
+    // Isso é um comentário de uma linha;
+    /*
+       Isso é um comentário
+       de multiplas linhas
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello devDojo!");
+    }
+}
